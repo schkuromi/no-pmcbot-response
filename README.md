04da53ee13f3d73b84c6b6cf17700f0605344df8
schkuromi <vgcrox@gmail.com>
2025-02-02 22:44:55 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# No PMC Bot Response
+
+## Features
+- Config option for changing the chance that a bot will send a message
+- One config option each, for when you are the killer, and when you are the victim
---
+++ src/mod.ts
@@ -0,0 +1,42 @@
+import { DependencyContainer } from "tsyringe";
+
+import { IPostSptLoadMod } from "@spt/models/external/IPostSptLoadMod";
+import { ILogger } from "@spt/models/spt/utils/ILogger";
+import { ConfigServer } from "@spt/servers/ConfigServer";
+import { ConfigTypes } from "@spt/models/enums/ConfigTypes";
+import { IPmcChatResponse } from "@spt/models/spt/config/IPmChatResponse";
+import { LogTextColor } from "@spt/models/spt/logging/LogTextColor";
+import { LogBackgroundColor } from "@spt/models/spt/logging/LogBackgroundColor";
+
+class Mod implements IPostSptLoadMod
+{
+    private modConfig = require("../config/config.json")
+
+    public postSptLoad(container: DependencyContainer): void
+    {
+        // get logger
+        const logger = container.resolve<ILogger>("WinstonLogger");
+
+        // get the config server so we can get a config with it
+        const configServer = container.resolve<ConfigServer>("ConfigServer");
+
+        // Request pmc bot response config
+        const responseConfig: IPmcChatResponse = configServer.getConfig<IPmcChatResponse>(ConfigTypes.PMC_CHAT_RESPONSE);
+
+        // change to response chance to config values
+        responseConfig.killer = this.modConfig.killedABotResponseChancePercent;
+        responseConfig.victim = this.modConfig.diedToABotResponseChancePercent;
+
+        // if debug, log values
+        if (this.modConfig.debug)
+        {
+            console.log("[DEBUG] [SCHKRM] PMC Bot Response Chance - Killer set to:",responseConfig.killer)
+            console.log("[DEBUG] [SCHKRM] PMC Bot Response Chance - Victim set to:",responseConfig.victim)
+        }
+
+        // announce mod loaded
+        logger.logWithColor("[SCHKRM] PMC Bot Response Chance tweaked.", LogTextColor.BLACK, LogBackgroundColor.YELLOW);
+    }
+}
+
+export const mod = new Mod();
